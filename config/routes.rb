de67593c8b5121e9e5adaa1cfeb4bd5142d748d2
Seth <Sethlee99@gmail.com>
2016-02-19 14:12:46 +0800

    v2

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/index'
+
   resources :passwords, controller: "clearance/passwords", only: [:create, :new]
   resource :session, controller: "clearance/sessions", only: [:create]
 
@@ -15,7 +17,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root "users#index"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
